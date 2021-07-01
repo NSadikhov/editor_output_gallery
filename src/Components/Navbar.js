65dd9776322f08eea1e5cd1ea7417dd3c93cd3c0
NSadikhov <nsadixov2000@gmail.com>
2021-07-02 01:38:03 +0200

    changes to navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,53 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Link } from 'react-router-dom';
 import classes from '../Styles/navbar.module.css';
 
 import ExitToAppRoundedIcon from '@material-ui/icons/ExitToAppRounded';
 
 const Navbar = () => {
+
+    const containerRef = useRef(null);
+
+    const handleToggle = (e) => {
+        const getAttributeValue = containerRef.current.getAttribute('data-toggle');
+        containerRef.current.setAttribute('data-toggle', getAttributeValue === 'open' ? 'closed' : 'open');
+
+        if (getAttributeValue === 'open') containerRef.current.style.minWidth = '60px';
+        else containerRef.current.style.minWidth = '250px';
+    }
+
+    const handleOnMouseDown = (e) => {
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+    }
+
+    const handleMouseMove = (e) => {
+        document.body.style.userSelect = 'none';
+        document.body.style.cursor = 'ew-resize';
+        const size = document.body.clientWidth - e.clientX;
+        if (size <= 250 && size >= 60) {
+            containerRef.current.style.transitionDuration = '0ms';
+            containerRef.current.style.minWidth = size + 'px';
+
+            if (size <= 125) containerRef.current.setAttribute('data-toggle', 'closed');
+            else containerRef.current.setAttribute('data-toggle', 'open');
+        }
+
+    }
+
+    const handleMouseUp = () => {
+        document.body.style.userSelect = 'auto';
+        document.body.style.cursor = 'default';
+        containerRef.current.style.transitionDuration = '300ms';
+        document.removeEventListener('mousemove', handleMouseMove);
+        document.removeEventListener('mouseup', handleMouseUp);
+    }
+
     return (
-        <div className={classes.container}>
-            <div className={classes.navBar}>
-                <ExitToAppRoundedIcon className={classes.icon} />
+        <div ref={containerRef} className={classes.container} data-toggle='open'>
+            <div className={classes.border} onMouseDown={handleOnMouseDown} />
+            <ExitToAppRoundedIcon onClick={handleToggle} className={classes.icon} />
+            <div className={classes.tabs}>
                 <Link to='/'>Editor & Output</Link>
                 <Link to='/gallery'>Gallery</Link>
             </div>
